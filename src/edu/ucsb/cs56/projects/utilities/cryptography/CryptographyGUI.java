bb00b1ffeec00009fb115c33c339c93ca0a23115
keithlo <kjlong14@gmail.com>
2016-11-07 19:35:09 -0800

    Update CryptographyGUI.java

--- src/edu/ucsb/cs56/projects/utilities/cryptography/CryptographyGUI.java
+++ src/edu/ucsb/cs56/projects/utilities/cryptography/CryptographyGUI.java
@@ -20,6 +20,19 @@ import java.awt.datatransfer.*;
 import java.awt.Toolkit;
 import javax.swing.JTextArea;
 
+import javax.swing.UIManager.*;
+
+try {
+    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+        if ("Nimbus".equals(info.getName())) {
+            UIManager.setLookAndFeel(info.getClassName());
+            break;
+        }
+    }
+} catch (Exception e) {
+    // If Nimbus is not available, you can set the GUI to another look and feel.
+}
+
 /**
    A class to implement the Cryptography GUI.
    @author Callum Steele
