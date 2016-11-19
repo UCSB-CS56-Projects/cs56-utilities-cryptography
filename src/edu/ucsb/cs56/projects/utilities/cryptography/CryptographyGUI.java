cdea8ae9ad5494767710797d9d41c9308ff43f6e
keithlo <kjlong14@gmail.com>
2016-11-19 12:42:57 -0800

    Update CryptographyGUI.java

--- src/edu/ucsb/cs56/projects/utilities/cryptography/CryptographyGUI.java
+++ src/edu/ucsb/cs56/projects/utilities/cryptography/CryptographyGUI.java
@@ -99,13 +99,13 @@ public class CryptographyGUI
 		    UIManager.setLookAndFeel("com.jtattoo.plaf.texture.TextureLookAndFeel");
 		    SwingUtilities.updateComponentTreeUI(frame);
 		} catch (ClassNotFoundException ex) {
-		    java.util.logging.Logger.getLogger(PC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		    //java.util.logging.Logger.getLogger(PC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		} catch (InstantiationException ex) {
-		    java.util.logging.Logger.getLogger(PC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		    //java.util.logging.Logger.getLogger(PC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		} catch (IllegalAccessException ex) {
-		    java.util.logging.Logger.getLogger(PC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		    //java.util.logging.Logger.getLogger(PC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
-		    java.util.logging.Logger.getLogger(PC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		    //java.util.logging.Logger.getLogger(PC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		}
 		
 		CryptographyGUI cryptoGUI = new CryptographyGUI();
