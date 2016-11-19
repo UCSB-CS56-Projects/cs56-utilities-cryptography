cee420d9dfd8fc0413d2d8f7f7b2ac70dd53f70e
keithlo <kjlong14@gmail.com>
2016-11-18 18:24:52 -0800

    Update CryptographyGUI.java

--- src/edu/ucsb/cs56/projects/utilities/cryptography/CryptographyGUI.java
+++ src/edu/ucsb/cs56/projects/utilities/cryptography/CryptographyGUI.java
@@ -93,13 +93,19 @@ public class CryptographyGUI
 	@param args Default arguments sent to main.
 	 */
 	public static void main (String[] args) {
-		try{	
-			UIManager.setLookAndFeel("com.jtattoo.plaf.smart.SmartLookAndFeel");
-		}catch (final UnsupportedLookAndFeelException e){
-			e.printstacktrace();
-		}catch (final ClassNotFoundException e){
-			e.printstacktrace();
+		try {
+		    //here you can put the selected theme class name in JTattoo
+		    UIManager.setLookAndFeel("com.jtattoo.plaf.texture.TextureLookAndFeel");
+		} catch (ClassNotFoundException ex) {
+		    java.util.logging.Logger.getLogger(PC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (InstantiationException ex) {
+		    java.util.logging.Logger.getLogger(PC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (IllegalAccessException ex) {
+		    java.util.logging.Logger.getLogger(PC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+		    java.util.logging.Logger.getLogger(PC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		}
+		
 		JFrame.setDefaultLookAndFeelDecorated(true);
 		CryptographyGUI cryptoGUI = new CryptographyGUI();
 
