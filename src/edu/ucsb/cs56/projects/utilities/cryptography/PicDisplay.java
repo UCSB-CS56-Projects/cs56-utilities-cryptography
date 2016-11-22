cbf9d0f1148e7244630f9eaf9cf624c3762c884a
keithlo <kjlong14@gmail.com>
2016-11-22 01:50:17 -0800

    Update PicDisplay.java

--- src/edu/ucsb/cs56/projects/utilities/cryptography/PicDisplay.java
+++ src/edu/ucsb/cs56/projects/utilities/cryptography/PicDisplay.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
